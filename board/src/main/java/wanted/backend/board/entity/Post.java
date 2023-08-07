827382f2d7d91657a5947e28f3ca199c44c59255
yoonjuho <dbswn990@naver.com>
2023-08-07 21:23:10 +0900

    feat : 게시글 객체 생성

---
+++ board/src/main/java/wanted/backend/board/entity/Post.java
@@ -0,0 +1,27 @@
+package wanted.backend.board.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
