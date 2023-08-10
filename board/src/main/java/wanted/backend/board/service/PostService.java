a981e7a6d6017d4babc81226fda53e448c3c4432
yoonjuho <dbswn990@naver.com>
2023-08-10 22:14:37 +0900

    refactor : 예외 메시지 변경

--- board/src/main/java/wanted/backend/board/service/PostService.java
+++ board/src/main/java/wanted/backend/board/service/PostService.java
@@ -3,7 +3,6 @@ package wanted.backend.board.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wanted.backend.board.dto.PostDetailResponse;
@@ -22,41 +21,41 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Transactional(readOnly = true)
-    public Page<PostResponse> searchAll(int page, int size){
-        PageRequest pageRequest = PageRequest.of(page-1, size);
+    public Page<PostResponse> searchAll(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
         return postRepository.findAllByOrderByUserIdDesc(pageRequest).map(PostResponse::from);
     }
 
     @Transactional
-    public void save(PostRequest postRequest, UserAdapter presentUser){
+    public void save(PostRequest postRequest, UserAdapter presentUser) {
         Post post = postRequest.toEntity(presentUser.getUser());
         postRepository.save(post);
     }
 
     @Transactional(readOnly = true)
-    public PostDetailResponse search(Long postId){
+    public PostDetailResponse search(Long postId) {
         Optional<Post> post = postRepository.findById(postId);
         return post.map(PostDetailResponse::from)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 ID입니다"));
     }
 
     @Transactional
-    public void update(Long postId, PostRequest postRequest, UserAdapter presentUser){
+    public void update(Long postId, PostRequest postRequest, UserAdapter presentUser) {
         Optional<Post> post = postRepository.findById(postId);
-        User user = post.map( o -> o.getUser())
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 ID입니다"));
-        if(!user.equals(presentUser.getUser())){
+        User user = post.map(Post::getUser)
+                .orElseThrow(() -> new RuntimeException("작성자가 존재하지 않는 게시물입니다"));
+        if (!user.equals(presentUser.getUser())) {
             throw new RuntimeException("게시글의 작성자만 수정할 수 있습니다");
         }
         post.get().update(postRequest.getTitle(), postRequest.getContent());
     }
 
     @Transactional
-    public void delete(Long postId, UserAdapter presentUser){
+    public void delete(Long postId, UserAdapter presentUser) {
         Optional<Post> post = postRepository.findById(postId);
-        User user = post.map( o -> o.getUser())
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 ID입니다"));
-        if(!user.equals(presentUser.getUser())){
+        User user = post.map(Post::getUser)
+                .orElseThrow(() -> new RuntimeException("작성자가 존재하지 않는 게시물입니다"));
+        if (!user.equals(presentUser.getUser())) {
             throw new RuntimeException("게시글의 작성자만 삭제할 수 있습니다");
         }
         postRepository.deleteById(postId);
